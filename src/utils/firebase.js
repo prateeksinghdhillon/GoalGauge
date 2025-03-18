004bd670e618a9ff4b76823f31f03ec712b784a6
prateeksinghdhillon <prateekdhillon58@gmail.com>
2025-03-18 13:19:03 +0530

    build comand updated

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,6 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
+console.log(process.env);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
